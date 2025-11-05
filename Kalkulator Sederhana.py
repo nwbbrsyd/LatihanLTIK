6fe2f98a7f9f4edc1adb3f38e1d15cfc229adf33
nwbbrsyd <nawab.rasyid@gmail.com>
2025-11-05 12:09:55 +0700

    inisial commit

---
+++ Kalkulator Sederhana.py
@@ -0,0 +1,6 @@
+angka1 = float(input("Masukkan angka 1 : "))
+angka2 = float(input("Masukkan angka 2 : "))
+angka3 = float(input("Masukkan angka 3 : "))
+
+hasil = angka1 + angka2 + angka3
+print(f"Hasil dari penjumlahan tersebut, {angka1} + {angka2} + {angka3} = {hasil}")
